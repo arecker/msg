6ff329c9fb3ad94a177b01ce6c7629ec765ff689
Alex Recker <alex@reckerfamily.com>
2015-09-04 23:39:30 -0500

    Tests: adds mock fabric to test server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,12 +1,25 @@
 from unittest import TestCase
 
-from fabric.api import env
-from fabric.exceptions import NetworkError
+from fabric import api
 
+from msg import server
 from msg.server import Accessor
 
 
-class TestAccessor(TestCase):
+class MockFabric(object):
+    def __init__(self):
+        self.env = {
+            'use_ssh_config': True
+        }
+        self.run = self._record
+        self.sudo = self._record
+
+    def _record(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+
+
+class TestAccessorEnvironment(TestCase):
     '''
     tests persistance of the server
     accessor in setting the fabric
@@ -17,7 +30,7 @@ class TestAccessor(TestCase):
         should set fabric to use
         ssh config
         '''
-        self.assertEqual(env['use_ssh_config'], True)
+        self.assertEqual(api.env['use_ssh_config'], True)
 
     def test_set_host(self):
         '''
@@ -25,7 +38,7 @@ class TestAccessor(TestCase):
         '''
         test_host_name = 'my_test_host'
         Accessor.host(test_host_name)
-        self.assertEqual(env['host_string'], test_host_name)
+        self.assertEqual(api.env['host_string'], test_host_name)
 
     def test_set_password(self):
         '''
@@ -33,7 +46,7 @@ class TestAccessor(TestCase):
         '''
         test_pass = 'monkey_balls'
         Accessor.password(test_pass)
-        self.assertEqual(env['password'], test_pass)
+        self.assertEqual(api.env['password'], test_pass)
 
     def test_password_host_chainable(self):
         '''
@@ -47,27 +60,33 @@ class TestAccessor(TestCase):
         Accessor.password(expected['pass']) \
                 .host(expected['host'])
 
-        self.assertEqual(env['host_string'], expected['host'])
-        self.assertEqual(env['password'], expected['pass'])
+        self.assertEqual(api.env['host_string'], expected['host'])
+        self.assertEqual(api.env['password'], expected['pass'])
+
+
+class TestAccessorCommands(TestCase):
+    '''
+    exercises command pass through to
+    fabric using mock fabric api
+    '''
+    def setUp(self):
+        server.api = self.mock = MockFabric()
+
+    def tearDown(self):
+        server.api = api
 
     def test_run(self):
-        try:
-            passed = True
-            Accessor.host('localhost')
-            Accessor.run('echo hello', timeout=5)
-        except NetworkError:
-            pass
-        except:
-            passed = False
-        self.assertTrue(passed)
+        Accessor.host('localhost')
+        Accessor.run('echo hello', timeout=5)
+        self.assertEqual(''.join(self.mock._args), ''.join(self.mock._args))
+        self.assertEqual(len(set(dict(timeout=5)) ^ set(self.mock._kwargs)), 0)
 
     def test_sudo(self):
-        try:
-            passed = True
-            Accessor.host('localhost')
-            Accessor.sudo('echo hello', timeout=5)
-        except NetworkError:
-            pass
-        except:
-            passed = False
-        self.assertTrue(passed)
+        Accessor.host('test-host').password('testpass')
+        Accessor.sudo('echo boo')
+        self.assertEqual(''.join(self.mock._args), ''.join(self.mock._args))
+        self.assertEqual(len(set(dict(
+            use_ssh_config=True,
+            password='testpass',
+            host_string='test-host'
+        )) ^ set(self.mock.env)), 0)

0f27595514822799bc80db4755c212657b28fc92
Alex Recker <alex@reckerfamily.com>
2015-09-05 15:54:25 -0500

    Test: adds another e2e test with overrides

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -47,3 +47,29 @@ servos:
             'echo "Hellooooooo nurse"',
             'sudo apt-get install -y python python-pip'
         ])
+
+
+class TestHandShakeInstallerConfigOverrides(MockMSGTestCase):
+    config_data = '''
+host:
+  prod: 'test-server'
+  stage: 'stage-server'
+
+servos:
+  - handshake:
+      hello: 'I am overriding the default message'
+  - install:
+      command: sudo pacman -S
+      packages:
+        - python
+        - python-pip
+        - mysql
+'''
+
+    def test_it(self):
+        Kicker(path=self.config).validate('prod').go()
+
+        self.assertEqual(self.mock.command_history, [
+            'echo "I am overriding the default message"',
+            'sudo pacman -S python python-pip mysql'
+        ])
